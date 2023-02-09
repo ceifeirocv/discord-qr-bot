8b3d30addb5949222437739b606821f8d5da05d7
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-02-09 18:09:51 -0100

    reverse node fs and path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
 require('dotenv/config')
 
